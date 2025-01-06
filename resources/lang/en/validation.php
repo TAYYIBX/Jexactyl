5f831808e9f918bc501bc56e5324be164ae493ca
TAYYIBX <raryhosting@gmail.com>
2025-01-06 09:05:20 +0800

    Update validation.php
    Change language to Malay

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -6,101 +6,101 @@ return [
     | Validation Language Lines
     |--------------------------------------------------------------------------
     |
-    | The following language lines contain the default error messages used by
-    | the validator class. Some of these rules have multiple versions such
-    | as the size rules. Feel free to tweak each of these messages here.
+    | Baris bahasa berikut mengandungi mesej ralat lalai yang digunakan oleh
+    | kelas validator. Beberapa peraturan ini mempunyai pelbagai versi seperti
+    | peraturan saiz. Anda bebas untuk mengubahsuai setiap mesej ini di sini.
     |
     */
 
-    'accepted' => 'The :attribute must be accepted.',
-    'active_url' => 'The :attribute is not a valid URL.',
-    'after' => 'The :attribute must be a date after :date.',
-    'after_or_equal' => 'The :attribute must be a date after or equal to :date.',
-    'alpha' => 'The :attribute may only contain letters.',
-    'alpha_dash' => 'The :attribute may only contain letters, numbers, and dashes.',
-    'alpha_num' => 'The :attribute may only contain letters and numbers.',
-    'array' => 'The :attribute must be an array.',
-    'before' => 'The :attribute must be a date before :date.',
-    'before_or_equal' => 'The :attribute must be a date before or equal to :date.',
+    'accepted' => ':attribute mesti diterima.',
+    'active_url' => ':attribute bukan URL yang sah.',
+    'after' => ':attribute mesti tarikh selepas :date.',
+    'after_or_equal' => ':attribute mesti tarikh selepas atau sama dengan :date.',
+    'alpha' => ':attribute hanya boleh mengandungi huruf.',
+    'alpha_dash' => ':attribute hanya boleh mengandungi huruf, nombor, dan sengkang.',
+    'alpha_num' => ':attribute hanya boleh mengandungi huruf dan nombor.',
+    'array' => ':attribute mesti menjadi tatasusunan.',
+    'before' => ':attribute mesti tarikh sebelum :date.',
+    'before_or_equal' => ':attribute mesti tarikh sebelum atau sama dengan :date.',
     'between' => [
-        'numeric' => 'The :attribute must be between :min and :max.',
-        'file' => 'The :attribute must be between :min and :max kilobytes.',
-        'string' => 'The :attribute must be between :min and :max characters.',
-        'array' => 'The :attribute must have between :min and :max items.',
+        'numeric' => ':attribute mesti antara :min dan :max.',
+        'file' => ':attribute mesti antara :min dan :max kilobytes.',
+        'string' => ':attribute mesti antara :min dan :max characters.',
+        'array' => ':attribute mesti mempunyai antara :min dan :max items.',
     ],
-    'boolean' => 'The :attribute field must be true or false.',
-    'confirmed' => 'The :attribute confirmation does not match.',
-    'date' => 'The :attribute is not a valid date.',
-    'date_format' => 'The :attribute does not match the format :format.',
-    'different' => 'The :attribute and :other must be different.',
-    'digits' => 'The :attribute must be :digits digits.',
-    'digits_between' => 'The :attribute must be between :min and :max digits.',
-    'dimensions' => 'The :attribute has invalid image dimensions.',
-    'distinct' => 'The :attribute field has a duplicate value.',
-    'email' => 'The :attribute must be a valid email address.',
-    'exists' => 'The selected :attribute is invalid.',
-    'file' => 'The :attribute must be a file.',
-    'filled' => 'The :attribute field is required.',
-    'image' => 'The :attribute must be an image.',
-    'in' => 'The selected :attribute is invalid.',
-    'in_array' => 'The :attribute field does not exist in :other.',
-    'integer' => 'The :attribute must be an integer.',
-    'ip' => 'The :attribute must be a valid IP address.',
-    'json' => 'The :attribute must be a valid JSON string.',
+    'boolean' => 'Ruangan :attribute mesti benar atau palsu.',
+    'confirmed' => 'Pengesahan :attribute tidak sepadan.',
+    'date' => ':attribute bukan tarikh yang sah.',
+    'date_format' => ':attribute tidak mengikut format :format.',
+    'different' => ':attribute dan :other mesti berbeza.',
+    'digits' => ':attribute mesti :digits digit.',
+    'digits_between' => ':attribute mesti antara :min dan :max digits.',
+    'dimensions' => ':attribute mempunyai dimensi imej yang tidak sah.',
+    'distinct' => 'Ruangan :attribute mempunyai nilai yang berulang.',
+    'email' => ':attribute mesti alamat emel yang sah.',
+    'exists' => ':attribute yang dipilih tidak sah.',
+    'file' => ':attribute mesti fail.',
+    'filled' => 'Ruangan :attribute mesti diisi.',
+    'image' => ':attribute mesti gambar.',
+    'in' => ':attribute yang dipilih tidak sah.',
+    'in_array' => 'Ruangan :attribute tidak wujud dalam :other.',
+    'integer' => ':attribute mesti nombor bulat.',
+    'ip' => ':attribute mesti alamat IP yang sah.',
+    'json' => ':attribute mesti rentetan JSON yang sah.',
     'max' => [
-        'numeric' => 'The :attribute may not be greater than :max.',
-        'file' => 'The :attribute may not be greater than :max kilobytes.',
-        'string' => 'The :attribute may not be greater than :max characters.',
-        'array' => 'The :attribute may not have more than :max items.',
+        'numeric' => ':attribute tidak boleh lebih daripada :max.',
+        'file' => ':attribute tidak boleh lebih daripada :max kilobytes.',
+        'string' => ':attribute tidak boleh lebih daripada :max characters.',
+        'array' => ':attribute tidak boleh mempunyai lebih daripada :max items.',
     ],
-    'mimes' => 'The :attribute must be a file of type: :values.',
-    'mimetypes' => 'The :attribute must be a file of type: :values.',
+    'mimes' => ':attribute mesti fail jenis: :values.',
+    'mimetypes' => ':attribute mesti fail jenis: :values.',
     'min' => [
-        'numeric' => 'The :attribute must be at least :min.',
-        'file' => 'The :attribute must be at least :min kilobytes.',
-        'string' => 'The :attribute must be at least :min characters.',
-        'array' => 'The :attribute must have at least :min items.',
+        'numeric' => ':attribute mesti sekurang-kurangnya :min.',
+        'file' => ':attribute mesti sekurang-kurangnya :min kilobytes.',
+        'string' => ':attribute mesti sekurang-kurangnya :min characters.',
+        'array' => ':attribute mesti mempunyai sekurang-kurangnya :min items.',
     ],
-    'not_in' => 'The selected :attribute is invalid.',
-    'numeric' => 'The :attribute must be a number.',
-    'present' => 'The :attribute field must be present.',
-    'regex' => 'The :attribute format is invalid.',
-    'required' => 'The :attribute field is required.',
-    'required_if' => 'The :attribute field is required when :other is :value.',
-    'required_unless' => 'The :attribute field is required unless :other is in :values.',
-    'required_with' => 'The :attribute field is required when :values is present.',
-    'required_with_all' => 'The :attribute field is required when :values is present.',
-    'required_without' => 'The :attribute field is required when :values is not present.',
-    'required_without_all' => 'The :attribute field is required when none of :values are present.',
-    'same' => 'The :attribute and :other must match.',
+    'not_in' => ':attribute yang dipilih tidak sah.',
+    'numeric' => ':attribute mesti nombor.',
+    'present' => 'Ruangan :attribute mesti hadir.',
+    'regex' => 'Format :attribute tidak sah.',
+    'required' => 'Ruangan :attribute diperlukan.',
+    'required_if' => 'Ruangan :attribute diperlukan apabila :other ialah :value.',
+    'required_unless' => 'Ruangan :attribute diperlukan kecuali :other ada dalam :values.',
+    'required_with' => 'Ruangan :attribute diperlukan apabila :values hadir.',
+    'required_with_all' => 'Ruangan :attribute diperlukan apabila :values hadir.',
+    'required_without' => 'Ruangan :attribute diperlukan apabila :values tidak hadir.',
+    'required_without_all' => 'Ruangan :attribute diperlukan apabila tiada :values hadir.',
+    'same' => ':attribute dan :other mesti sepadan.',
     'size' => [
-        'numeric' => 'The :attribute must be :size.',
-        'file' => 'The :attribute must be :size kilobytes.',
-        'string' => 'The :attribute must be :size characters.',
-        'array' => 'The :attribute must contain :size items.',
+        'numeric' => ':attribute mesti :size.',
+        'file' => ':attribute mesti :size kilobytes.',
+        'string' => ':attribute mesti :size characters.',
+        'array' => ':attribute mesti mengandungi :size items.',
     ],
-    'string' => 'The :attribute must be a string.',
-    'timezone' => 'The :attribute must be a valid zone.',
-    'unique' => 'The :attribute has already been taken.',
-    'uploaded' => 'The :attribute failed to upload.',
-    'url' => 'The :attribute format is invalid.',
+    'string' => ':attribute mesti rentetan.',
+    'timezone' => ':attribute mesti zon waktu yang sah.',
+    'unique' => ':attribute telah digunakan.',
+    'uploaded' => ':attribute gagal dimuat naik.',
+    'url' => 'Format :attribute tidak sah.',
 
     /*
     |--------------------------------------------------------------------------
     | Custom Validation Attributes
     |--------------------------------------------------------------------------
     |
-    | The following language lines are used to swap attribute place-holders
-    | with something more reader friendly such as E-Mail Address instead
-    | of "email". This simply helps us make messages a little cleaner.
+    | Baris bahasa berikut digunakan untuk menukar pemegang tempat atribut
+    | dengan sesuatu yang lebih mesra pembaca seperti "Alamat Emel" menggantikan
+    | "email". Ini membantu menjadikan mesej lebih jelas.
     |
     */
 
     'attributes' => [],
 
-    // Internal validation logic for Jexactyl
+    // Logik validasi dalaman untuk Jexactyl
     'internal' => [
         'variable_value' => ':env variable',
-        'invalid_password' => 'The password provided was invalid for this account.',
+        'invalid_password' => 'Kata laluan yang diberikan tidak sah untuk akaun ini.',
     ],
 ];
